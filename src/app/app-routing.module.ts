61957c0e8a24a6f8a6632848c2e9157cfe50d284
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-07-22 22:34:01 -0500

    app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'credit-card',
+    loadChildren: () => import('./credit-card/credit-card.module').then(m => m.CreditCardModule)
+  },
+  {
+    path: '',
+    redirectTo: 'credit-card',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'credit-card',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
