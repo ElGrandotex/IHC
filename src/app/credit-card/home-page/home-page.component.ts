d00f28c726b8e51f60829779afc1c9dd96803836
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-08-05 11:40:40 -0500

    HomePage component

--- src/app/credit-card/home-page/home-page.component.ts
+++ src/app/credit-card/home-page/home-page.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { User } from '../../interface/user.interface';
-import { CreditCardService } from '../services/credit-card.service';
+import { CreditCardService } from '../../services/credit-card.service';
 import { MatTableDataSource } from '@angular/material/table';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent  implements OnInit{
+export class HomePageComponent implements OnInit {
 
-  public users: User[] = [];
+  private localStorage = this.document.defaultView?.localStorage;
   usersList = new MatTableDataSource<User>();
   displayedColumns: string[] = ['id', 'name', 'creditNumber', 'pago'];
+  private token: string | null = this.localStorage!.getItem('token');
 
-  constructor(private creditService: CreditCardService) { }
+
+  constructor(
+    private creditService: CreditCardService,
+    @Inject(DOCUMENT) private document: Document,
+  ) { }
 
   ngOnInit(): void {
     this.loadData();
@@ -25,19 +31,11 @@ export class HomePageComponent  implements OnInit{
   }
 
   public loadData() {
-    this.creditService.getUsers()
+    this.creditService.getUsers(this.token)
       .subscribe(
-        data => {
-          let csvToRowArray = data.split("\n");
-          for (let index = 1; index < csvToRowArray.length - 1; index++) {
-            let row = csvToRowArray[index].split(",");
-            this.users.push(new User(parseInt(row[0]), row[1], parseInt(row[2]), parseInt(row[3]), row[4]==='true'));
-          }
-          this.usersList.data = this.users
-        },
-        error => {
-          console.log(error);
+        (response: any) => {
+          this.usersList = response;
         }
-      );
+      )
   }
 }
