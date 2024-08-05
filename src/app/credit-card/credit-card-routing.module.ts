14971e2b67203d9fccdb57a6832008fc931651d2
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-08-05 11:39:30 -0500

    CreditCard Module

--- src/app/credit-card/credit-card-routing.module.ts
+++ src/app/credit-card/credit-card-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './layout-page/layout-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { PagoComponent } from './pago/pago.component';
+import { BeneficiosComponent } from './beneficios/beneficios.component';
 
 const routes: Routes = [
   {
@@ -9,6 +11,8 @@ const routes: Routes = [
     component: LayoutPageComponent,
     children: [
       { path: 'home', component: HomePageComponent},
+      { path: 'pago', component: PagoComponent},
+      { path: 'beneficio', component: BeneficiosComponent},
       { path: '**', redirectTo:'home'}
     ]
   }
